5e6bd69ceb122df7728d26164cfde696da74db36
Jeffrey Jaca <jeffreyjjaca@gmail.com>
2022-06-20 16:27:49 -0400

    updated wordpress module main.tf

--- modules/wordpress/main.tf
+++ modules/wordpress/main.tf
@@ -25,36 +25,6 @@ resource "docker_container" "wp" {
 	}
 }
 
-resource "docker_container" "lara" {
-  count = var.cms_option ? 0 : 1
-  image = "laratest"
-  name = "lara_container"
-	#networks_advanced {
-		#   name = docker_network.wp_net.name
-		#}
-
-	# laravel must be served on port 80 to run on localhost
-  command = [
-    "php", "artisan", "serve", "--host=0.0.0.0", "--port=80",
-  ]
-
-  env = [
-		#   "APP_URL=http://lara.test",
-    "DB_HOST=${var.db_instance_name}:3306",
-    "DB_USERNAME=${var.db_user}",
-    "DB_PASSWORD=${var.db_user_pass}",
-    "DB_DATABASE=${var.db_name}",
-		#"DB_PORT=3306",
-    "DB_CONNECTION=mysql",
-  ]
-
-  ports {
-    internal = var.published_port
-    external = var.host_port
-  }
-}
-
-
 #resource "docker_volume" "db_data" {}
 
 #resource "docker_network" "wp_net" {
